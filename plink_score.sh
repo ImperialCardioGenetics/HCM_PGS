2efdcf50df208815afbec6cfe212f8612989e71f
seanlzheng <53749449+seanlzheng@users.noreply.github.com>
2023-07-20 23:00:01 +0100

    Add files via upload

---
+++ plink_score.sh
@@ -0,0 +1,4 @@
+plink \
+--bfile /rds/general/project/lms-ware-analysis/live/sean/prs/HCM/target/UKBB/UKBB_GWAS_MERGE_INCLUDING_RELATED.QC \
+--score /rds/general/project/lms-ware-analysis/live/sean/prs/PRScs/HCM/output/posterior_snp_effect_estimates/hcm.mtag.exc.mybpc3.1kg.inc.related.merge.txt 2 4 6 \
+--out /rds/general/project/lms-ware-analysis/live/sean/prs/PRScs/HCM/output/scores/PRScs.HCM.MTAG.MYBPC3.UKBB.1KG.LD.INC.RELATED
---
+++ prscs.sh
@@ -0,0 +1,6 @@
+python /rds/general/project/lms-ware-analysis/live/sean/prs/PRScs/PRScs.py \
+--ref_dir=/rds/general/project/lms-ware-analysis/live/sean/prs/PRScs/1kg_reference/ldblk_1kg_eur \
+--bim_prefix=/rds/general/project/lms-ware-analysis/live/sean/prs/HCM/target/UKBB/UKBB_GWAS_MERGE_INCLUDING_RELATED.QC \
+--sst_file=/rds/general/project/lms-ware-analysis/live/sean/gwama_sum_stats/HCM_MTAG/HCM_MTAG.QC.prscs.format.exc.mybpc3.txt \
+--n_gwas=74259 \
+--out_dir=/rds/general/project/lms-ware-analysis/live/sean/prs/PRScs/HCM/output/posterior_snp_effect_estimates/hcm.mtag.exc.mybpc3.1kg.ld.inc.related
